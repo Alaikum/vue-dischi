bc50a6ec5a320a2709b0b7103d9343a9f104ed96
Alaikum <carangiadamo@gmail.com>
2022-09-07 17:43:33 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text-center">ciao</div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../src/style/general.scss";
+
+</style>
